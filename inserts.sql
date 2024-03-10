cb47db6e21b051f447f0105cce72f9668506a610
SirFrancis2007 <132942551+SirFrancis2007@users.noreply.github.com>
2024-03-10 19:41:25 -0300

    Commit 1

--- inserts.sql
+++ inserts.sql
@@ -174,5 +174,36 @@ START TRANSACTION ;
                 (15, 2, 10, 'El viaje de Chihiro', '00:01:37')
 ;
     
-    
+	INSERT INTO Actor_Pelicula (idActor, idPelicula)
+	values
+		-- Frozen 1
+		(1,1)
+		(2,1)
+		(3,1)
+		(4,1)
+		(5,1)
+		(6,1)
+		(7,1)
+		(8,1)
+		-- Frozen 2
+		(1,2)
+		(2,2)
+		(3,2)
+		(4,2)
+		(13,2)
+		(14,2)
+		(15,2)
+		(16,2)
+		-- interestellar
+		(17,9)
+		(18,9)
+		(19,9)
+		(20,9)
+		(21,9)
+		(22,9)
+		(23,9)
+		(24,9)
+		(25,9)
+		(26,9)
+		(27,9)
 commit;
