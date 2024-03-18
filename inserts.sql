efc74f28bc00c6649c9ec1635326c7fb9c142bc5
SirFrancis2007 <132942551+SirFrancis2007@users.noreply.github.com>
2024-03-18 20:37:07 -0300

    Agregar el der correccion de inserts

--- inserts.sql
+++ inserts.sql
@@ -49,7 +49,7 @@ START TRANSACTION ;
 ;
 
     
-    insert into Actor (idActor, idPelicula, Nombre, Apellido, fecha_nacimiento, sexo, nacionalidad, Rol, Personaje)
+		insert into Actor (idActor, idPelicula, Nombre, Apellido, fecha_nacimiento, sexo, nacionalidad, Rol, Personaje)
 	Values  -- Frozen 1
 			(1, 1, 'Kristen ', 'Bell', '1980-07-18', 'F', 'Estadounidense', 'Actriz de voz', 'Anna'),
 			(2, 1, 'Idina ', 'Menzel', '1971-05-30', 'F', 'Estadounidense', 'Actriz de voz', 'Elsa'),
@@ -59,91 +59,83 @@ START TRANSACTION ;
 			(6, 1, 'Eva ', 'Bella', '2002-06-04', 'F', 'Estadounidense', 'Actriz de voz', 'Joven Elsa'),
 			(7, 1, 'Livvy ', 'Stubenraunch', '2005-04-18', 'F', 'Estadounidense', 'Actriz de voz', 'Joven Anna'),
 			(8, 1, 'Santino ', 'Fontana', '1982-03-21', 'M', 'Estadounidense', 'Actor de voz', 'Hans'),
-            -- Fronzen 2
-			(9, 2, 'Kristen ', 'Bell', '1980-07-18', 'F', 'Estadounidense', 'Actriz de voz', 'Anna'),
-			(10, 2, 'Idina ', 'Menzel', '1971-05-30', 'F', 'Estadounidense', 'Actriz de voz', 'Elsa'),
-			(11, 2, 'Jonathan ', 'Groff', '1995-03-26', 'M', 'Estadounidense', 'Actor de voz', 'Kristoff'),
-			(12, 2, 'Josh ', 'Gad', '1981-02-23', 'M', 'Estadounidense', 'Actor de voz', 'Olaf'),
-			(13, 2, 'Sterling ', 'Brown', '1976-04-05', 'M', 'Estadounidense', 'Actor de voz', 'Matthias'),
-			(14, 2, 'Evan ', 'Wood', '1987-09-07', 'F', 'Estadounidense', 'Actriz de voz', 'Iduna'),
-			(15, 2, 'Martha ', 'Plimpton', '1970-11-16', 'F', 'Estadounidense', 'Actriz de voz', 'Yelana'),
-			(16, 2, 'Rachel ', 'Matthews', '1993-10-25', 'F', 'Estadounidense', 'Actriz de voz', 'Honeymaren'),
-            -- Interestelar
-			(17, 9, 'Matthew ', 'McConaughey', '1969-11-04', 'M', 'Estadounidense', 'Actor Live action', 'Joseph Cooper'),
-			(18, 9, 'Anne ', 'Hathaway', '1982-11-12', 'F', 'Estadounidense', 'Actriz Live action', 'Amelia Brand'),
-			(19, 9, 'Mackenzie ', 'Foy', '2000-11-10', 'F', 'Estadounidense', 'Actriz Live action', 'Murph joven'),
-			(20, 9, 'Jessica ', 'Chastain', '1977-03-24', 'F', 'Estadounidense', 'Actriz Live action', 'Murph adulta'),
-			(21, 9, 'Michael ', 'Caine', '1933-03-14', 'M', 'Británico', 'Actor Live Action', 'Profesor Brand'),
-			(22, 9, 'Casey', 'Affleck', '1975-08-12', 'M', 'Estadounidense', 'Actor Live Action', 'Tom'),
-			(23, 9, 'Matt', 'Damon', '1970-10-08', 'M', 'Estadounidense', 'Actor Live Action', 'Dr. Mann'),
-			(24, 9, 'John', 'Lithgow', '1945-10-19', 'M', 'Estadounidense', 'Actor Live Action', 'Donald'),
-			(25, 9, 'Ellen', 'Burstyn', '1932-12-07', 'F', 'Estadounidense', 'Actriz Live action', 'Murph anciana'),
-			(26, 9, 'David', 'Gyasi', '1980-01-02', 'M', 'Británica', 'Actor Live Action', 'Romilly'),
-			(27, 9, 'Topher', 'Grace', '1978-07-12', 'M', 'Estadounidense', 'Actor Live Action', 'Getty'),
-			(28, 9, 'Timothée', 'Chalamet', '1995-12-27', 'M', 'Estadounidense', 'Actor Live Action', 'Tom joven'),
-			(29, 9, 'Josh', 'Stewart', '1977-02-06', 'M', 'Estadounidense', 'Actor de voz', 'CASE'),
-            -- El viaje de Chihiro
-		    (30, 10, 'Hiiragi', 'Rumi', '1987-08-01', 'F', 'Japonesa', 'Actriz de voz', 'Chihiro Ogino'),
-			(31, 10, 'Hirino', 'Miyu', '1998-02-19', 'M', 'Japonés', 'Actor de voz', 'Haku'),
-			(32, 10, 'Yoomi', 'Tamai', '1977-08-06', 'M', 'Japonesa', 'Actor de voz', 'Lin'),
-			(33, 10, 'Ryūnosuke', 'Kamiki', '1993-05-19', 'M', 'Japonés', 'Actor de voz', 'Boh'),
-			(34, 10, 'Natsuki', 'Mari', '1952-05-02', 'F', 'Japonesa', 'Actriz de voz', 'Yubaba/Zeniba'),
-			(35, 10, 'Sugawara', 'Bunta', '1933-08-16', 'M', 'Japonés', 'Actor de voz', 'Kamaji'),
-			(36, 10, 'Takashi', 'Naito', '1955-05-28', 'M', 'Japonés', 'Actor de voz', 'Akio Ogino'),
-			(37, 10, 'Yasuko', 'Sawaguchi', '1965-06-28', 'F', 'Japonesa', 'Actriz de voz', 'Yugo Ogino'),
-			(38, 10, 'Akio', 'Nakamura', '1960-03-03', 'M', 'Japonés', 'Actor de voz', 'Sin Cara'),
-			(39, 10, 'Tsunehiko', 'Kamijo', '1940-03-07', 'M', 'Japonés', 'Actor de voz', 'Chichiyaku'),
-			(40, 10, 'Koba', 'Hayashi', '1935-07-27', 'M', 'Japonés', 'Actor de voz', 'Espíritu del Río'),
-			(41, 10, 'Tatsuya', 'Gashuin', '1950-12-10', 'M', 'Japonés', 'Actor de voz', 'Aoegeru'),
-            -- El extraño mundo de jack
-            (42, 7,'Chiris', 'Sarandon', '1942-07-24', 'M', 'Estadounidense', 'Actor de voz', 'Jack Skellington'),
-            (43, 7,'Catherine', 'O´Hara', '1954-03-04', 'F', 'Estadounidense', 'Actriz de voz', 'Sally'),
-            (44, 7,'Glenn', 'Shadix', '1953-04-15', 'M', 'Estadounidense', 'Actor de voz', 'Alcalde'),
-            (45, 7,'Ken', 'Page', '1954-01-20', 'M', 'Estadounidense', 'Actor de voz', 'Oogie Boogie'),
-            -- El gato con botas 1
-			(46, 5, 'Antonio ', 'Bandera', '1960-08-10', 'M', 'Español', 'Actor de voz', 'El gato con botas'),
-			(47, 5, 'Salma ', 'Hayek', '1996-09-02', 'F', 'Mexicana', 'Actriz de voz', 'Kitty payitas suave'),
-			(48, 5, 'Zacharius', 'Galifianakis', '1969-11-01', 'M', 'Estadounidense', 'Actor de voz', 'Humpty Dumpty'),
-			(49, 5, 'Billy Bob', 'Thornton', '1955-08-04', 'M', 'Estadounidense', 'Actor de voz', 'Jack'),
-			(50, 5, 'Amy', 'Sedaris', '1961-03-29', 'F', 'Estadounidense', 'Actriz de voz', 'Jill'),
-			(51, 5, 'Guillermo', 'Del Toro', '1964-10-09', 'M', 'Mexicano', 'Actor de voz', 'Comandante'),
-			(52, 5, 'Constance', 'Marie', '1969-09-09', 'F', 'Estadounidense', 'Actriz de voz', 'Imelda'),
-			(53, 5, 'Rich', 'Dietl', '1980-07-21', 'M', 'Estadounidense', 'Actor de voz', 'Bounty hunter'),
-            -- El gato con botas: el ultimo deseo
-			(54, 6, 'Antonio ', 'Bandera', '1960-08-10', 'M', 'Español', 'Actor de voz', 'El gato con botas'),
-			(55, 6, 'Salma ', 'Hayek', '1996-09-02', 'F', 'Mexicana', 'Actriz de voz', 'Kitty payitas suave'),
-			(56, 6, 'Harvey', 'Guillen', '1990-05-03', 'M', 'Estadounidense', 'Actor de voz', 'Perro'),
-			(57, 6, 'Wagner ', 'Moura', '1976-06-27', 'M', 'Brasileño', 'Actor de voz', 'Lobo'),
-			(58, 6, 'Wagner', 'Moura', '1976-06-27', 'M', 'Brasileño', 'Actor de voz', 'Bebe oso'),
-			(59, 6, 'Florence', 'Pugh', '1996-01-03', 'F', 'Británica', 'Actriz de voz', 'Ricitos de oro'),
-			(60, 6, 'Olivia', 'Colman', '1974-01-30', 'F', 'Británica', 'Actriz de voz', 'Mama oso'),
-			(61, 6, 'Ray', 'Winstone', '1957-02-19', 'M', 'Británico', 'Actor de voz', 'Papa oso'),
-            -- Megalodon
-            (62, 3, 'Jason ', 'Statham', '1967-07-26', 'M', 'Britanico', 'Actor Live Action', 'Jonas Taylor'),
-            (63, 3, 'Bingbing', 'Li', '1973-02-27', 'F','China', 'Actriz Live Action', 'Suyin'), 
-            (64, 3,'Rainn', 'Wilson', '1968-01-20', 'M', 'Estadounidense', 'Actor Live Action', 'Morris'),
-            (65, 3, 'Cliff ', 'Curtis', '1968-07-27', 'M', 'Neozelandes', 'Actor Live Action', 'Mac'),
-            (66, 3, 'Winston', 'Chao', '1960-06-09','M', 'Taiwanes', 'Actor Live Action', 'Zhang'),
-            (67, 3, 'Ruby', 'Rose', '1986-03-20', 'F', 'Australiana', 'Actriz Live Action', 'Jaxx'),
-            (68, 3, 'Page ', 'Kennedy', '1976-11-22', 'M', 'Estadounidense', 'Actor Live Action', 'DJ'),
-            (69, 3, 'Robert', 'Taylor II', '1963-11-12', 'M','Australiano', 'Actor Live Action','Heller'),
+			-- Fronzen 2
+			(9, 2, 'Sterling ', 'Brown', '1976-04-05', 'M', 'Estadounidense', 'Actor de voz', 'Matthias'),
+			(10, 2, 'Evan ', 'Wood', '1987-09-07', 'F', 'Estadounidense', 'Actriz de voz', 'Iduna'),
+			(11, 2, 'Martha ', 'Plimpton', '1970-11-16', 'F', 'Estadounidense', 'Actriz de voz', 'Yelana'),
+			(12, 2, 'Rachel ', 'Matthews', '1993-10-25', 'F', 'Estadounidense', 'Actriz de voz', 'Honeymaren'),
+			-- Interestelar
+			(13, 9, 'Matthew ', 'McConaughey', '1969-11-04', 'M', 'Estadounidense', 'Actor Live action', 'Joseph Cooper'),
+			(14, 9, 'Anne ', 'Hathaway', '1982-11-12', 'F', 'Estadounidense', 'Actriz Live action', 'Amelia Brand'),
+			(15, 9, 'Mackenzie ', 'Foy', '2000-11-10', 'F', 'Estadounidense', 'Actriz Live action', 'Murph joven'),
+			(16, 9, 'Jessica ', 'Chastain', '1977-03-24', 'F', 'Estadounidense', 'Actriz Live action', 'Murph adulta'),
+			(17, 9, 'Michael ', 'Caine', '1933-03-14', 'M', 'Británico', 'Actor Live Action', 'Profesor Brand'),
+			(18, 9, 'Casey', 'Affleck', '1975-08-12', 'M', 'Estadounidense', 'Actor Live Action', 'Tom'),
+			(19, 9, 'Matt', 'Damon', '1970-10-08', 'M', 'Estadounidense', 'Actor Live Action', 'Dr. Mann'),
+			(20, 9, 'John', 'Lithgow', '1945-10-19', 'M', 'Estadounidense', 'Actor Live Action', 'Donald'),
+			(21, 9, 'Ellen', 'Burstyn', '1932-12-07', 'F', 'Estadounidense', 'Actriz Live action', 'Murph anciana'),
+			(22, 9, 'David', 'Gyasi', '1980-01-02', 'M', 'Británica', 'Actor Live Action', 'Romilly'),
+			(23, 9, 'Topher', 'Grace', '1978-07-12', 'M', 'Estadounidense', 'Actor Live Action', 'Getty'),
+			(24, 9, 'Timothée', 'Chalamet', '1995-12-27', 'M', 'Estadounidense', 'Actor Live Action', 'Tom joven'),
+			(25, 9, 'Josh', 'Stewart', '1977-02-06', 'M', 'Estadounidense', 'Actor de voz', 'CASE'),
+			-- El viaje de Chihiro
+			(26, 10, 'Hiiragi', 'Rumi', '1987-08-01', 'F', 'Japonesa', 'Actriz de voz', 'Chihiro Ogino'),
+			(27, 10, 'Hirino', 'Miyu', '1998-02-19', 'M', 'Japonés', 'Actor de voz', 'Haku'),
+			(28, 10, 'Yoomi', 'Tamai', '1977-08-06', 'M', 'Japonesa', 'Actor de voz', 'Lin'),
+			(29, 10, 'Ryūnosuke', 'Kamiki', '1993-05-19', 'M', 'Japonés', 'Actor de voz', 'Boh'),
+			(30, 10, 'Natsuki', 'Mari', '1952-05-02', 'F', 'Japonesa', 'Actriz de voz', 'Yubaba/Zeniba'),
+			(31, 10, 'Sugawara', 'Bunta', '1933-08-16', 'M', 'Japonés', 'Actor de voz', 'Kamaji'),
+			(32, 10, 'Takashi', 'Naito', '1955-05-28', 'M', 'Japonés', 'Actor de voz', 'Akio Ogino'),
+			(33, 10, 'Yasuko', 'Sawaguchi', '1965-06-28', 'F', 'Japonesa', 'Actriz de voz', 'Yugo Ogino'),
+			(34, 10, 'Akio', 'Nakamura', '1960-03-03', 'M', 'Japonés', 'Actor de voz', 'Sin Cara'),
+			(35, 10, 'Tsunehiko', 'Kamijo', '1940-03-07', 'M', 'Japonés', 'Actor de voz', 'Chichiyaku'),
+			(36, 10, 'Koba', 'Hayashi', '1935-07-27', 'M', 'Japonés', 'Actor de voz', 'Espíritu del Río'),
+			(37, 10, 'Tatsuya', 'Gashuin', '1950-12-10', 'M', 'Japonés', 'Actor de voz', 'Aoegeru'),
+			-- El extraño mundo de jack
+			(38, 7,'Chiris', 'Sarandon', '1942-07-24', 'M', 'Estadounidense', 'Actor de voz', 'Jack Skellington'),
+			(39, 7,'Catherine', 'O´Hara', '1954-03-04', 'F', 'Estadounidense', 'Actriz de voz', 'Sally'),
+			(40, 7,'Glenn', 'Shadix', '1953-04-15', 'M', 'Estadounidense', 'Actor de voz', 'Alcalde'),
+			(41, 7,'Ken', 'Page', '1954-01-20', 'M', 'Estadounidense', 'Actor de voz', 'Oogie Boogie'),
+			-- El gato con botas 1
+			(42, 5, 'Antonio ', 'Bandera', '1960-08-10', 'M', 'Español', 'Actor de voz', 'El gato con botas'),
+			(43, 5, 'Salma ', 'Hayek', '1996-09-02', 'F', 'Mexicana', 'Actriz de voz', 'Kitty payitas suave'),
+			(43, 5, 'Zacharius', 'Galifianakis', '1969-11-01', 'M', 'Estadounidense', 'Actor de voz', 'Humpty Dumpty'),
+			(44, 5, 'Billy Bob', 'Thornton', '1955-08-04', 'M', 'Estadounidense', 'Actor de voz', 'Jack'),
+			(45, 5, 'Amy', 'Sedaris', '1961-03-29', 'F', 'Estadounidense', 'Actriz de voz', 'Jill'),
+			(46, 5, 'Guillermo', 'Del Toro', '1964-10-09', 'M', 'Mexicano', 'Actor de voz', 'Comandante'),
+			(47, 5, 'Constance', 'Marie', '1969-09-09', 'F', 'Estadounidense', 'Actriz de voz', 'Imelda'),
+			(48, 5, 'Rich', 'Dietl', '1980-07-21', 'M', 'Estadounidense', 'Actor de voz', 'Bounty hunter'),
+			-- El gato con botas: el ultimo deseo
+			(49, 6, 'Harvey', 'Guillen', '1990-05-03', 'M', 'Estadounidense', 'Actor de voz', 'Perro'),
+			(50, 6, 'Wagner ', 'Moura', '1976-06-27', 'M', 'Brasileño', 'Actor de voz', 'Lobo'),
+			(51, 6, 'Wagner', 'Moura', '1976-06-27', 'M', 'Brasileño', 'Actor de voz', 'Bebe oso'),
+			(52, 6, 'Florence', 'Pugh', '1996-01-03', 'F', 'Británica', 'Actriz de voz', 'Ricitos de oro'),
+			(53, 6, 'Olivia', 'Colman', '1974-01-30', 'F', 'Británica', 'Actriz de voz', 'Mama oso'),
+			(54, 6, 'Ray', 'Winstone', '1957-02-19', 'M', 'Británico', 'Actor de voz', 'Papa oso'),
+			-- Megalodon
+			(55, 3, 'Jason ', 'Statham', '1967-07-26', 'M', 'Britanico', 'Actor Live Action', 'Jonas Taylor'),
+			(56, 3, 'Bingbing', 'Li', '1973-02-27', 'F','China', 'Actriz Live Action', 'Suyin'),
+			(57, 3,'Rainn', 'Wilson', '1968-01-20', 'M', 'Estadounidense', 'Actor Live Action', 'Morris'),
+			(58, 3, 'Cliff ', 'Curtis', '1968-07-27', 'M', 'Neozelandes', 'Actor Live Action', 'Mac'),
+			(59, 3, 'Winston', 'Chao', '1960-06-09','M', 'Taiwanes', 'Actor Live Action', 'Zhang'),
+			(60, 3, 'Ruby', 'Rose', '1986-03-20', 'F', 'Australiana', 'Actriz Live Action', 'Jaxx'),
+			(61, 3, 'Page ', 'Kennedy', '1976-11-22', 'M', 'Estadounidense', 'Actor Live Action', 'DJ'),
+			(62, 3, 'Robert', 'Taylor II', '1963-11-12', 'M','Australiano', 'Actor Live Action','Heller'),
 			-- Megalodon II
-            (70, 4, 'Jason ', 'Statham', '1967-07-26', 'M', 'Britanico', 'Actor Live Action', 'Jonas Taylor'),
-            (71, 4, 'Jing', 'Wu', '1974-04-03', 'F', 'China', 'Atriz live Action', 'Jiu Ming Zhang'),
-            (72, 4, 'Shuya', 'Sophia Cai', '2008-04-22', 'F', 'China', 'Atriz live Action', 'Meiying'),
-            (73, 4, 'Page ', 'Kennedy', '1976-11-22', 'M', 'Estadounidense', 'Actor Live Action', 'DJ'),
-            (74, 4, 'Segio', 'Peris-Mencheta', '1975-04-07', 'M', 'Español', 'Actor Live Action', 'Montes'),
-            (75, 4, 'Skyler', 'Samuels', '1994-04-14', 'F', 'Estadounidense', 'Actor Live Action', 'Jess'),
-            (76, 4, 'Cliff ', 'Curtis', '1968-07-27', 'M', 'Neozelandes', 'Actor Live Action', 'Mac'),
-            (77, 4, 'Melissanthi', 'Mahut', '1988-09-20', 'F', 'Canadiense', 'Atriz live Action', 'Rigas'),
+			(63, 4, 'Jing', 'Wu', '1974-04-03', 'F', 'China', 'Atriz live Action', 'Jiu Ming Zhang'),
+			(65, 4, 'Shuya', 'Sophia Cai', '2008-04-22', 'F', 'China', 'Atriz live Action', 'Meiying'),
+			(66, 4, 'Segio', 'Peris-Mencheta', '1975-04-07', 'M', 'Español', 'Actor Live Action', 'Montes'),
+			(67, 4, 'Skyler', 'Samuels', '1994-04-14', 'F', 'Estadounidense', 'Actor Live Action', 'Jess'),
+			(68, 4, 'Melissanthi', 'Mahut', '1988-09-20', 'F', 'Canadiense', 'Atriz live Action', 'Rigas'),
 			-- El origen de los guardianes:
-			(78, 8, 'Christopher ', 'Pine', '1980-08-26', 'M', 'Estadouniodense', 'Actor de voz', 'Jack Fros'),
-            (79, 8, 'David Jude', 'Law', '1972-12-29', 'M', 'Estadouniodense', 'Actor de voz', 'Pitch Black'),
-            (80, 8, 'Michael', 'Jackman', '1968-10-12', 'M', 'Estadouniodense', 'Actor de voz', 'Conejo de pascuas'),
-            (81, 8, 'Alexander', 'Baldwin', '1958-04-03', 'M', 'Estadouniodense', 'Actor de voz', 'Santa Claus'),
-            (82, 8, 'Isla', 'Fisher', '1976-02-03', 'F', 'Australiana', 'Actor de voz', 'Hada de los dientes'),
-            (83, 8, 'Dakota ', 'Goyo', '1999-08-22', 'M', 'Canadiense', 'Actor de voz', 'Jamie Bennet')
-    ;
+			(69, 8, 'Christopher ', 'Pine', '1980-08-26', 'M', 'Estadouniodense', 'Actor de voz', 'Jack Fros'),
+			(70, 8, 'David Jude', 'Law', '1972-12-29', 'M', 'Estadouniodense', 'Actor de voz', 'Pitch Black'),
+			(71, 8, 'Michael', 'Jackman', '1968-10-12', 'M', 'Estadouniodense', 'Actor de voz', 'Conejo de pascuas'),
+			(72, 8, 'Alexander', 'Baldwin', '1958-04-03', 'M', 'Estadouniodense', 'Actor de voz', 'Santa Claus'),
+			(73, 8, 'Isla', 'Fisher', '1976-02-03', 'F', 'Australiana', 'Actor de voz', 'Hada de los dientes'),
+			(74, 8, 'Dakota ', 'Goyo', '1999-08-22', 'M', 'Canadiense', 'Actor de voz', 'Jamie Bennet')
+	;
+
     
     insert into Saga(idSaga, Numero_Saga, idPelicula, Nombre)
     values
@@ -174,36 +166,101 @@ START TRANSACTION ;
                 (15, 2, 10, 'El viaje de Chihiro', '00:01:37')
 ;
     
-	INSERT INTO Actor_Pelicula (idActor, idPelicula)
-	values
+	insert into Actor_Pelicula (idActor, idPelicula)
+        values
 		-- Frozen 1
-		(1,1)
-		(2,1)
-		(3,1)
-		(4,1)
-		(5,1)
-		(6,1)
-		(7,1)
-		(8,1)
+			(1,1)
+			(2,1)
+			(3,1)
+			(4,1)
+			(5,1)
+			(6,1)
+			(7,1)
+			(8,1)
 		-- Frozen 2
-		(1,2)
-		(2,2)
-		(3,2)
-		(4,2)
-		(13,2)
-		(14,2)
-		(15,2)
-		(16,2)
-		-- interestellar
-		(17,9)
-		(18,9)
-		(19,9)
-		(20,9)
-		(21,9)
-		(22,9)
-		(23,9)
-		(24,9)
-		(25,9)
-		(26,9)
-		(27,9)
+			(1,2)
+			(2,2)
+			(3,2)
+			(4,2)
+			(9,2)
+			(10,2)
+			(11,2)
+			(12,2)
+		-- Interesellar
+			(13, 9)
+			(14, 9)
+			(15, 9)
+			(16, 9)
+			(17, 9)
+			(18, 9)
+			(19, 9)
+			(20, 9)
+			(21, 9)
+			(22, 9)
+			(23, 9)
+			(24, 9)
+			(25, 9)
+		-- El viaje de Chihiro
+			(26, 10)
+			(27, 10)
+			(28, 10)
+			(29, 10)
+			(30, 10)
+			(31, 10)
+			(32, 10)
+			(33, 10)
+			(34, 10)
+			(35, 10)
+			(36, 10)
+			(37, 10)
+		-- El extraño mundo de Jack
+			(38, 7)
+			(39, 7)
+			(40, 7)
+			(41, 7)
+		-- El gato con botas
+			(42, 5)
+			(43, 5)
+			(43, 5)
+			(44, 5)
+			(45, 5)
+			(46, 5)
+			(47, 5)
+			(48, 5)
+		-- EL gato con botas: El ultimo deseo
+			(42, 6)
+			(43, 6)
+			(49, 6)
+			(50, 6)
+			(51, 6)
+			(52, 6)
+			(53, 6)
+			(54, 6)
+		-- Megalodon 1
+			(55, 3)
+			(56, 3)
+			(57, 3)
+			(58, 3)
+			(59, 3)
+			(60, 3)
+			(61, 3)
+			(62, 3)
+		-- Megalodon 2
+			(55, 4)
+			(58, 4)
+			(61, 4)
+			(63, 4)
+			(65, 4)
+			(66, 4)
+			(67, 4)
+			(68, 4)
+		-- El origen de los guardianes
+			(69, 8)
+			(70, 8)
+			(71, 8)
+			(72, 8)
+			(73, 8)
+			(74, 8)
+;
+
 commit;
---
+++ readme.md
@@ -0,0 +1,91 @@
+erDiagram
+
+
+Pelicula{
+
+
+mediumint_unsigned idpelicula PK
+int_unsigned id_produccion FK
+tinyint_unsigned id_genero FK
+tinyint_unsigned id_actor  FK
+varchar(40) Nombre
+date Estreno
+varchar(300) Descripcion
+tinyint_unsigned calificacion
+TIME duracion
+tinyint_unsigned Restrincion
+Biging_Unsigned Recaudado
+}
+
+
+Produccion{
+int_unsigned id_produccion PK
+mediumint_unsigned id_pelicula FK
+int_unsigned  id_Estudio Fk
+varchar(50) Director_General
+varchar(100) guion
+varchar(100) Productor
+varchar(100) Vestuario
+varchar(100) Sonido
+float Presupuesto
+varchar(100) musica
+}
+
+
+Trailer{
+   tinyint_unsigned ID_Trailer PK
+   tinyint_unsigned id_genero FK
+   varchar(50) Nombre
+   TIME duracion
+   mediumint_unsigned idpelicula FK
+}
+
+
+Genero{
+   tinyint_unsigned id_genero PK
+   varchar(30) Genero
+}
+
+
+Saga{
+   tinyint_unsigned IdSaga PK
+   tinyint_unsigned id_Pelicula FK
+   tinyint_unsigned Numero_Saga
+   varchar(50) Nombre
+}
+
+
+Actor{
+   int_unsigned id_actor PK
+   mediumint_unsigned idPelicula FK
+   varchar(40) Nombre
+   varchar(40) apellido
+   date fecha_nacimiento
+   char(1) sexo
+   varchar(40) nacionalidad
+   varchar(40) Rol
+}
+
+
+Actor_Pelicula{
+   int_unsigned id_actor fk,pk
+   mediumint id_Pelicula fk,pk
+}
+
+
+Estudio{
+   int_unsigned id_Estudio pk
+   varchar(15) Nombre uk
+   year anio_fundacion
+}
+
+
+Actor}|--||Actor_Pelicula:""
+Pelicula}|--||Actor_Pelicula:""
+Pelicula||--}|Trailer:""
+Pelicula}|--o|Saga:""
+Pelicula}|--||Produccion:""
+Estudio||--|{Produccion:""
+Genero}|--||Trailer:""
+
+
